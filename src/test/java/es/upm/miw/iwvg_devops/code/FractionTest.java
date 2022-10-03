033f122c293dcc4ff4b0f1d3ce1fd6d6da358667
1257198442 <1257198442@qq.com>
2022-10-03 12:44:52 +0200

    FractionTest Enhancement #11

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -31,4 +31,44 @@ public class FractionTest {
         fraction=new Fraction(1,2);
         assertEquals(0.5,this.fraction.decimal());
     }
+
+    @Test
+    void  testIsproper(){
+        assertEquals(true,this.fraction.isProper());
+    }
+
+    @Test
+    void testisImproper(){
+        this.fraction.setNumerator(100);
+        this.fraction.setDenominator(1);
+        assertEquals(true,this.fraction.isImproper());
+    }
+
+    @Test
+    void  testisEquivalent(){
+        Fraction new_fraction=new Fraction(10,1000);
+        assertEquals(true,this.fraction.isEquivalent(new_fraction));
+    }
+
+
+    @Test
+    void  testAdd(){
+        Fraction new_fraction1=new Fraction(99,100);
+        Fraction new_fraction2=new Fraction(100,100);
+        assertEquals(true,this.fraction.add(new_fraction1).isEquivalent(new_fraction2));
+    }
+
+    @Test
+    void  testMultiply(){
+        Fraction new_fraction1=new Fraction(2,3);
+        Fraction new_fraction2=new Fraction(2,300);
+        assertEquals(true,this.fraction.multiply(new_fraction1).isEquivalent(new_fraction2));
+    }
+
+    @Test
+    void testDicide(){
+        Fraction new_fraction1=new Fraction(1,100);
+        Fraction new_fraction2=new Fraction(100,100);
+        assertEquals(true,this.fraction.divide(new_fraction1).isEquivalent(new_fraction2));
+    }
 }
